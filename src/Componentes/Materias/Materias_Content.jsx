40ba6dd0bad3ab376c25aed4614313848a499943
NachoDaCosta <nacho5981@gmail.com>
2024-03-03 22:38:17 -0300

    arreglo de errores de medidas mq

--- src/Componentes/Materias/Materias_Content.jsx
+++ src/Componentes/Materias/Materias_Content.jsx
@@ -6,99 +6,111 @@ const Materias2 = ({ infoMateria, mostrarInformacionMateria }) => {
       <h1>Materias</h1>
 
       <div className="mats">
-        <div
+        <a
+          href="#infoContainer"
           className="materia-info"
           onClick={() => mostrarInformacionMateria("CDIV")}
         >
           <img src="/imgs/c1.webp" alt="" />
           <span>CDIV</span>
-        </div>
+        </a>
 
-        <div
+        <a
+          href="#infoContainer"
           className="materia-info"
           onClick={() => mostrarInformacionMateria("CDIVV")}
         >
           <img src="/imgs/c2.webp" alt="" />
           <span>CDIVV</span>
-        </div>
+        </a>
 
-        <div
+        <a
+          href="#infoContainer"
           className="materia-info"
           onClick={() => mostrarInformacionMateria("CALCULO VECTORIAL")}
         >
           <img src="/imgs/c3.webp" alt="" />
           <span>Calc Vectorial</span>
-        </div>
+        </a>
 
-        <div
+        <a
+          href="#infoContainer"
           className="materia-info"
           onClick={() => mostrarInformacionMateria("FISICA 1")}
         >
           <img src="/imgs/f1.webp" alt="" />
           <span>Fisica 1</span>
-        </div>
+        </a>
 
-        <div
+        <a
+          href="#infoContainer"
           className="materia-info"
           onClick={() => mostrarInformacionMateria("FISICA 2")}
         >
           <img src="/imgs/f2.webp" alt="" />
           <span>Fisica 2</span>
-        </div>
+        </a>
 
-        <div
+        <a
+          href="#infoContainer"
           className="materia-info"
           onClick={() => mostrarInformacionMateria("GAL 1")}
         >
           <img src="/imgs/g1.webp" alt="" />
           <span>Gal 1</span>
-        </div>
-        <div
+        </a>
+        <a
+          href="#infoContainer"
           className="materia-info"
           onClick={() => mostrarInformacionMateria("GAL 2")}
         >
           <img src="/imgs/g2.webp" alt="" />
           <span>Gal 2</span>
-        </div>
+        </a>
 
-        <div
+        <a
+          href="#infoContainer"
           className="materia-info"
           onClick={() => mostrarInformacionMateria("MATEMATICA DISCRETA 1")}
         >
           <img src="/imgs/m1.webp" alt="" />
           <span>Matematica Discreta 1</span>
-        </div>
+        </a>
 
-        <div
+        <a
+          href="#infoContainer"
           className="materia-info"
           onClick={() => mostrarInformacionMateria("MATEMATICA DISCRETA 2")}
         >
           <img src="/imgs/m2.webp" alt="" />
           <span>Matematica Discreta 2</span>
-        </div>
+        </a>
 
-        <div
+        <a
+          href="#infoContainer"
           className="materia-info"
           onClick={() => mostrarInformacionMateria("PROGRAMACION 1")}
         >
           <img src="/imgs/p1.webp" alt="" />
           <span>Programación 1</span>
-        </div>
+        </a>
 
-        <div
+        <a
+          href="#infoContainer"
           className="materia-info"
           onClick={() => mostrarInformacionMateria("PROGRAMACION 2")}
         >
           <img src="/imgs/p2.webp" alt="" />
           <span>Programación 2</span>
-        </div>
-        <div
+        </a>
+        <a
+          href="#infoContainer"
           className="materia-info"
           onClick={() => mostrarInformacionMateria("MATEMATICA INICIAL")}
         >
           <img src="/imgs/mi.webp" alt="" />
           <span>Matematica Inicial</span>
-        </div>
+        </a>
       </div>
       <div id="infoContainer">
         <div className="title-materia">
