321ad8c352fc0f713756a474465c3880737a1060
NachoDaCosta <nacho5981@gmail.com>
2024-11-03 11:47:41 -0300

    v0.2

--- src/Componentes/Footer/Footer.jsx
+++ src/Componentes/Footer/Footer.jsx
@@ -7,7 +7,9 @@ const Footer = () => {
       <div>&copy; Hecho por Nacho Da Costa v0.1</div>
       <div className="trabajo">
         <div>Si sabes react y quieres ayudar</div>
-        <img alt="logo-wpp-footer" src="./imgs/whatsapp.webp" />
+        <a href="https://wa.me/59893853398?text=hola%2C%20vengo%20por%20lo%20de%20react">
+          <img alt="logo-wpp-footer" src="./imgs/whatsapp.webp" />
+        </a>
       </div>
     </footer>
   );
